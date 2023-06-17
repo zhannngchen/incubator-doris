f0bc3b968aea539d4599585fa89a4987ea5e2ea6
Chen Zhang <chzhang1987@gmail.com>
2023-06-17 23:05:16 +0800

    Revert "[enhancement](page cache) use separate pk index cache (#19864)"
    This reverts commit c0ad5888010d3fb9c97197ffd8b92d47578dde45.

--- be/src/olap/rowset/segment_v2/indexed_column_reader.h
+++ be/src/olap/rowset/segment_v2/indexed_column_reader.h
@@ -65,7 +65,6 @@ public:
 
     CompressionTypePB get_compression() const { return _meta.compression(); }
     uint64_t get_memory_size() const { return _mem_size; }
-    void set_is_pk_index(bool is_pk) { _is_pk_index = is_pk; }
 
 private:
     Status load_index_page(const PagePointerPB& pp, PageHandle* handle, IndexPageReader* reader);
@@ -92,7 +91,6 @@ private:
     const EncodingInfo* _encoding_info = nullptr;
     const KeyCoder* _value_key_coder = nullptr;
     uint64_t _mem_size = 0;
-    bool _is_pk_index = false;
 };
 
 class IndexedColumnIterator {
@@ -131,6 +129,8 @@ public:
     // After one seek, we can only call this function once to read data
     Status next_batch(size_t* n, vectorized::MutableColumnPtr& dst);
 
+    void set_is_pk_index(bool is_pk) { _is_pk_index = is_pk; }
+
 private:
     Status _read_data_page(const PagePointer& pp);
 
@@ -149,6 +149,8 @@ private:
     ordinal_t _current_ordinal = 0;
     // iterator owned compress codec, should NOT be shared by threads, initialized before used
     BlockCompressionCodec* _compress_codec = nullptr;
+
+    bool _is_pk_index = false;
 };
 
 } // namespace segment_v2
