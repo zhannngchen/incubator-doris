0157da133d306b2f2fd9d286c712de3174664cd8
Chen Zhang <zhangchen@selectdb.com>
2025-05-07 23:34:31 +0800

    add log

--- be/test/olap/delta_writer_test.cpp
+++ be/test/olap/delta_writer_test.cpp
@@ -704,8 +704,10 @@ TEST_F(TestDeltaWriter, vec_write) {
         // get the value from sequence column
         int64_t date_v16= read_block.get_by_position(16).column->get_int(0);
         std::cout << "date_v_16: " << date_v16 << ", size:" << sizeof(date_v16) << std::endl;
+        std::cout << "date_v_16 raw: " << read_block.get_by_position(16).column->get_raw_data();
         int64_t date_v17= read_block.get_by_position(17).column->get_int(0);
         std::cout << "date_v_17: " << date_v17 << ", size:" << sizeof(date_v17)  << std::endl;
+        std::cout << "date_v_17 raw: " << read_block.get_by_position(17).column->get_raw_data();
     }
     ASSERT_EQ(1, tablet->num_rows());
 
